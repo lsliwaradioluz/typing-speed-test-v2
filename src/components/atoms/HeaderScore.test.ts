77c9f4ba81404c0e164b5fe91d50d2217d911644
Lukasz Sliwa <lsliwaradioluz@gmail.com>
2020-11-13 13:52:58 +0100

    create headerScore atom with tests

---
+++ src/components/atoms/HeaderScore.test.ts
@@ -0,0 +1,21 @@
+import HeaderScore from "./HeaderScore.vue";
+import { shallowMount } from "@vue/test-utils";
+
+interface Score {
+  number: number;
+  caption: string;
+}
+
+describe("HeaderScore", () => {
+  let propsData: Score = {
+    number: 0,
+    caption: "Random number",
+  };
+
+  it("renders without errors", () => {
+    const wrapper = shallowMount(HeaderScore, {
+      propsData,
+    });
+    expect(wrapper.exists()).toBe(true);
+  });
+});
---
+++ src/components/atoms/HeaderScore.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header-word-data">
+    <h2 class="number">{{ number }}</h2>
+    <p class="caption">
+      {{ caption }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  props: {
+    number: {
+      type: Number,
+      required: true,
+    },
+    caption: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.header-word-data {
+  text-align: center;
+  color: white;
+}
+
+.number {
+  font-size: 3rem;
+}
+
+.caption {
+  font-size: 0.9rem;
+  text-transform: uppercase;
+}
+</style>
